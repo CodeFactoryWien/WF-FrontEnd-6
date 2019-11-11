24ef2a6744c4b3d3a303d7568c0046606cb3cc1c
Sebastian Malcher <sebastianlukasmalcher@gmail.com>
2019-11-11 15:02:54 +0100

    adds basic js

--- js/course.js
+++ js/course.js
@@ -1,6 +1,22 @@
 //---------------------------------------Contact------Builder--------------------------------------------------
 function initCourse(){
-	console.log("ich bin die startfunciton einer unterseite. diese wird aus der navbar aufgerufen, die zuvor den content leert und alles was hier kommt einfügt. die funtkion die aufgerufen wird beginnt mit 'init' und endet mit dem namen der unterseite.")
+
 }	
 
 //---------------------------------------End contact Builder---------------------------------------------------
+{/* <div class="card mb-4 box-shadow">
+				<img class="card-img-top mx-auto img-fluid" src="./img/course/frontend.jpg" data-holder-rendered="true" style="width: 40%; display: block;">
+				<div class="card-body pt-0 row">
+					<div class="col-12 display-4 text-dark px-0 pt-0 pb-1 my-4">
+						<p class="col text-center p-1 my-0 mx-auto">Full Stack Web Developer</p>
+					</div>
+					<p class="col-12 col-md-9 card-text lead text-dark px-4 mx-auto" style="line-height: 1.5">Der CodeFactory Full Stack Web Developer Lehrgang bereitet dich auf deinen vielfältigen Einsatz als Junior Web Developer vor. Du lernst von Back-End über Front-End Anwendungen die gängigsten Programmiersprachen und Frameworks kennen und einsetzen, wie z.B. Angular, PHP, WordPress, HTML, CSS, jQuery und JavaScript. Als Full Stack Web Developer verfügst du über umfassende Kenntnisse im Bereich Softwareentwicklung und beherrschst aktuell relevante Programmiersprachen. Dadurch kannst du in der Web- aber auch in der Softwareentwicklung tätig sein. Als Generalist kannst du mit deinen Coding-Fähigkeiten fast jede Aufgabe lösen und arbeitest oft eng mit Projektleitungen zusammen, indem du dich um Anforderungsmanagement, Software- und Test-Planung sowie um Programmiertätigkeiten kümmerst.</p>
+
+					<div class="col-10 col-md-7 col-lg-4 mx-auto my-2 mt-lg-5 row">
+						<a class="btn btn-secondary text-info btn-lg active mx-auto p-2 pb-3 px-4 w-100 placeBtn" role="button" aria-pressed="true">Basic</a>
+					</div>
+					<div class="col-10 col-md-7 col-lg-4 mx-auto my-2 mt-lg-5 row">
+						<a class="btn btn-secondary text-warning btn-lg active mx-auto p-2 pb-3 px-4 w-100 eventBtn" role="button" aria-pressed="true">Advanced</a>
+					</div>
+				</div>
+			</div> */}
