d9c27e1eaa8cbca971d726d39ad6e20c0a963c2b
wf-Jessica <triltsch.jessica@hotmail.com>
2019-11-11 15:34:22 +0100

    team

---
+++ js/team.js
@@ -0,0 +1,43 @@
+function initCourse(){}
+	body.innerHTML+=`<div>
+		<h3>Trainer</h3>
+		<div>
+    <img src="" alt="">
+			<p>Trainer</p>
+			
+		</div>
+		<div>
+			<img src="" alt="">
+			<p>Trainer</p>
+			
+		</div>
+		
+	</div>
+	<div>
+		<h3>Students</h3>
+		<div>
+			<img src="" alt="">
+			<p>Trainer</p>
+			
+		</div>
+		<div>
+			<img src="" alt="">
+			<p>Trainer</p>
+			
+		</div>
+		<div>
+			<img src="" alt="">
+			<p>Trainer</p>
+			
+		</div>
+		<div>
+			<img src="" alt="">
+			<p>Trainer</p>
+			
+		</div>
+
+		
+	</div>
+	<div>
+		
+	</div>`
