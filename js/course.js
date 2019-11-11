9b24d3c9b362d91e487cbcd1d98c17f81dc4f61f
roman-cf <roman.springer@outlook.com>
2019-11-11 11:52:45 +0100

    js strucktur vorbereitet

---
+++ js/course.js
@@ -0,0 +1,6 @@
+//---------------------------------------Contact------Builder--------------------------------------------------
+function initCourse(){
+	console.log("ich bin die startfunciton einer unterseite. diese wird aus der navbar aufgerufen, die zuvor den content leert und alles was hier kommt einfügt. die funtkion die aufgerufen wird beginnt mit 'init' und endet mit dem namen der unterseite.)
+}	
+
+//---------------------------------------End contact Builder---------------------------------------------------
--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 //-------------------------------------------Site Builder---------------------------------------------------
 	$("title").text("The New Codefactory");
 	$("body").append(`
-		<header>header</header>
-		<nav>navbar kommt hier hin</nav>
+		<header>header
+			<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+			</nav>
+		</header>
 		<main>
 			hier liegt der hauptinhalt der seite, der von der navbar aus gelöscht und neubefüllt wird indem für die jeweilige unterseite eine function aufgerufen wird, die im jeweiligen seitenname.js liegt. Dafür müssen wir noch eine Namenskonvention festlegen.
 		</main>
