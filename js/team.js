6f5dc101166d4efa30f93844ac73dfd8e28771e4
wf-Jessica <triltsch.jessica@hotmail.com>
2019-11-12 10:52:41 +0100

    team

--- js/team.js
+++ js/team.js
@@ -5,45 +5,100 @@ function initTeam(){
 
 function displayTeam(){
 	document.querySelector("main").innerHTML+=`<div>
-		<h3>Trainer</h3>
-		<div>
-    <img src="" alt="">
-			<p>Trainer</p>
-			
-		</div>
-		<div>
-			<img src="" alt="">
-			<p>Trainer</p>
-			
-		</div>
-		
-	</div>
-	<div>
-		<h3>Students</h3>
-		<div>
-			<img src="" alt="">
-			<p>Trainer</p>
-			
-		</div>
-		<div>
-			<img src="" alt="">
-			<p>Trainer</p>
-			
-		</div>
-		<div>
-			<img src="" alt="">
-			<p>Trainer</p>
-			
-		</div>
-		<div>
-			<img src="" alt="">
-			<p>Trainer</p>
-			
-		</div>
+		<h1 class=" text-light mt-2 rounded text-center">TRAINER</h1>
+		<div class="container marketing">
+		<div class="row ml-auto">
+			<div class=" col-lg-5 col-md-10">
+			<div class=" card h-100">
+			<img src="img/team/theo.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title">Theodora Patkos</h3>
+			<p class="card-text">Trainer</p>
+			</div>
+			</div>
+			</div>
 
+			<div class=" col-lg-5 col-md-10">
+			<div class=" card h-100">
+			<img src="img/team/goran.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title ">Goran Stevic</h3>
+			<p class="card-text ">Trainer</p>
+			</div>
+			</div>
+			</div>
 		
 	</div>
-	<div>
+	<h1 class="text-light mt-2 rounded text-center">STUDENTS</h1>
+	<div class="row">
 		
+	<div class="row">
+			<div class=" col-lg-3 col-md-5">
+			<div class=" card h-100">
+			<img src="img/team/theo.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title">Jessica Triltsch</h3>
+			<p class="card-text">Teamleader<br> Teampage</p>
+			</div>
+			</div>
+			</div>
+			<div class="col-lg-3 col-md-5">
+			<div class=" card h-100">
+			<img src="img/team/theo.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title">Jasmine Wagner</h3>
+			<p class="card-text">Contact Page <br> News Page</p>
+			</div>
+			</div>
+			</div>
+			<div class="col-lg-3 col-md-5">
+			<div class=" card h-100">
+			<img src="img/team/theo.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title">Roman Springer</h3>
+			<p class="card-text">Landing Page <br> Chief Designer</p>
+			</div>
+			</div>
+			</div>
+			<div class="col-lg-3 col-md-5">
+			<div class=" card h-100">
+			<img src="img/team/theo.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title">Sebastian Malcher</h3>
+			<p class="card-text">Course Master</p>
+			</div>
+			</div>
+			</div>
+		
+	</div>
+	
+</div>
+<h1 class=" text-light mt-2 rounded text-center">OUR STUDENTS SAY</h1>
+		<div class="container marketing">
+		<div class="row ml-auto">
+			<div class=" col-lg-5 col-md-10">
+			<div class=" card h-100">
+			<img src="img/team/benjamin.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title">Benjamnin (21)</h3>
+			<p class="card-text">In comparison with Front-End or Back-end courses only, it IS 2x the time and 2x the price BUT I think it is important to get the full education so that you have access to all opportunities. It is just too limiting to do just the Front End or just the Back End. It is also much faster than a regular degree programme – it is definitely for people who want to enter the labor market faster. It is challenging, but also interesting</p>
+			</div>
+			</div>
+			</div>
+
+			<div class=" col-lg-5 col-md-10">
+			<div class=" card h-100">
+			<img src="img/team/judith.jpg" class="card-img-top" alt="theo">
+			<div class="card-body text-center">
+			<h3 class="card-title ">Judith (40)</h3>
+			<p class="card-text ">I chose CodeFactory because it is much more focused – it is not burdened by unnecessary expensive courses on unnecessary side topics. It targets specifically what I am missing in my education. After all, in a modern managerial environment I should be able to talk to programmers.</p>
+			</div>
+			</div>
+			</div>
+		
+	</div>
+
+	
+	</div>
 	</div>`
 }
