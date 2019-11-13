1112aae0b234fee3ad3329b955a8eb3e39eacce6
Jasmine Wagner <jasminewagner@hotmail.com>
2019-11-13 11:39:01 +0100

    update

--- js/news.js
+++ js/news.js
@@ -1,17 +1,15 @@
 
-function initNews() {
-	renderNews();
-}
+function initNews(card_id) { renderNews(card_id); }
 
-function renderNews() {
-
-document.querySelector("main").innerHTML += `
-<h1 class=" text-light mt-2 rounded text-center">NEWS</h1>
-<div class="card-columns"></div>`;
+function renderNews(card_id) {
+	
+	document.querySelector("main").innerHTML = `
+	<h1 class=" text-light mt-2 rounded text-center">NEWS</h1>
+	<div class="card-columns"></div>`;
 
 	newsJson.forEach(function(news) {
 		document.querySelector(".card-columns").innerHTML += `
-		  <div class="card shadow">
+		  <div class="card shadow" id="${news.newsId}">
 			<img src="${news.picUrl}" class="card-img-top" width="100%" height="auto" style="object-fit: cover">
 			<div class="card-body">
 			  <a href="${news.link}" target="_blank"><h5 class="card-title">${news.title}</h5></a>
@@ -20,4 +18,15 @@ document.querySelector("main").innerHTML += `
 		  </div>
 		`;
 	});
+
+	for(i=0; i<newsJson.length; i++){
+		if(card_id == news.newsId) {
+			document.getElementById(news.newsId).classList.add("active");
+			
+				$("html, body").animate({
+					scrollTop: $(".active").offset().top
+				}, 600);
+		}
+	}
 }
+
