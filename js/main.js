759b400edd2b2fc8003c2cc3cf5109f0851b089f
wf-Jessica <triltsch.jessica@hotmail.com>
2019-11-14 14:05:09 +0100

    hajshjka

--- js/main.js
+++ js/main.js
@@ -55,46 +55,48 @@ function initHome(){
 
 		<main class="container" id="mainDiv"></main>  
 
-		<footer class="p-4  border-top" >
+		<footer class="pt-4 mb-0  border-top" >
 			<div class="container">
-				<ul class="list-unstyled list-inline text-center">
-				      <li class="list-inline-item">
-				        <a class="btn-floating btn-fb mx-1">
-				          <i class="fa fa-facebook opacity"> </i>
-				        </a>
-				      </li>
-				      <li class="list-inline-item">
-				        <a class="btn-floating btn-tw mx-1">
-				          <i class="fa fa-twitter opacity"> </i>
-				        </a>
-				      </li>
-				      <li class="list-inline-item">
-				        <a class="btn-floating btn-gplus mx-1">
-				          <i class="fa fa-google opacity"> </i>
-				        </a>
-				      </li>
-				      <li class="list-inline-item">
-				        <a class="btn-floating btn-li mx-1">
-				          <i class="fa fa-linkedin opacity"> </i>
-				        </a>
-				      </li>
-				      <li class="list-inline-item">
-				        <a class="btn-floating btn-dribbble mx-1">
-				          <i class="fa fa-instagram opacity"> </i>
-				        </a>
-				      </li>
-				    </ul>
+			    
+	
+<ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fa fa-facebook opacity"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fa fa-twitter opacity"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fa fa-google opacity"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-li mx-1">
+          <i class="fa fa-linkedin opacity"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-dribbble mx-1">
+          <i class="fa fa-instagram opacity"> </i>
+        </a>
+      </li>
+    </ul>
 
-					 </div>
-  
-
-				  <!-- Copyright -->
-				  <div class="footer-copyright text-center py-3">© 2019 Copyright: J<sub>2</sub>RS GmbH
-				  </div>
-				  <!-- Copyright -->
-
-			    </div>
+  </div>
+  </div>
 			</div>
+
+  <!-- Copyright -->
+  <div class="ft footer-copyright text-center py-3">© 2019 Copyright: J<sub>2</sub>RS GmbH
+  </div>
+  <!-- Copyright -->
+
+			    
 		</footer>
 	`);
 	home();
--- js/news.js
+++ js/news.js
@@ -5,7 +5,6 @@ function initNews(card_id) {
 
 function renderNews(card_id) {
 	document.querySelector("main").innerHTML = `
-
 	<h1 class=" text-light rounded text-center">NEWS</h1>
 	<div class="container">
 		<div class="card-columns"></div>
